e6a19112fc3c5209f190459b13e254a5a455523a
Infocatcher <infocatcher.dev.null@gmail.com>
2019-02-04 09:01:54 +0300

    Import: show files count in checkbox label (#32)

--- chrome/content/sets.js
+++ chrome/content/sets.js
@@ -3292,10 +3292,19 @@ var handyClicksSets = {
 			this.$("hc-sets-tree-buttonImportOk").focus();
 	},
 	setImportFilesDataStatus: function() {
+		var files = this.ps.files;
+		var countFD = 0;
+		for(var path in files) if(files.hasOwnProperty(path))
+			++countFD;
+		var noFD = !countFD;
 		var importFD = this.$("hc-sets-tree-importFilesData");
-		var noFD = this.ut.isEmptyObj(this.ps.files);
 		importFD.disabled = noFD;
 		importFD.checked = this._importFilesData = !noFD;
+		var label = countFD
+			? importFD.getAttribute("hc_labelN").replace("$n", countFD)
+			: importFD.getAttribute("hc_label");
+		if(importFD.getAttribute("label") != label)
+			importFD.setAttribute("label", label);
 	},
 	cleanImportSearch: function() {
 		var search = this.searchField.value;
